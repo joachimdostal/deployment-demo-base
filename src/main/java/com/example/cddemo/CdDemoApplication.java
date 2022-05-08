d9b1284630ac6b20cddc7bd3a181acde38cd76bb
Dominik Kress <dk.dkress@gmail.com>
2022-05-08 14:57:15 +0200

    fix some naming issues

--- src/main/java/com/example/cddemo/CdDemoApplication.java
+++ src/main/java/com/example/cddemo/CdDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.ctdemo;
+package com.example.cddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CtDemoApplication {
+public class CdDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CtDemoApplication.class, args);
+		SpringApplication.run(CdDemoApplication.class, args);
 	}
 
 }
